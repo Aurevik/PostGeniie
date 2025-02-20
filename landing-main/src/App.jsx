af6b70562cb061442d87ae1504e0b5ef86fa4ecf
Ayush Kumar <ayush129kumar@gmail.com>
2025-02-20 19:13:27 +0530

    update

--- landing-main/src/App.jsx
+++ landing-main/src/App.jsx
@@ -1,14 +1,25 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
-import * as Sentry from '@sentry/react';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
+import * as Sentry from "@sentry/react";
+
+// Replace with your actual GA4 Measurement ID
+const TRACKING_ID = "G-8PSNRLQ8HV"; 
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.send("pageview"); // Tracks page views
+  }, []);
+
   return (
     <main className="bg-black">
       <Navbar />
@@ -19,7 +30,7 @@ const App = () => {
       <HowItWorks />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
 export default Sentry.withProfiler(App);
