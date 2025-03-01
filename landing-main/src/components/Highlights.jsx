be41ba6af4c810439fa8e1fed43e749b291b2f30
Maneesh Amera <maneesh.amera@advertalyst.ai>
2025-03-01 11:27:20 +0530

    box added

--- landing-main/src/components/Highlights.jsx
+++ landing-main/src/components/Highlights.jsx
@@ -1,7 +1,5 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { rightImg, watchImg } from "../utils"
-
 import VideoCarousel from './VideoCarousel';
 
 const Highlights = () => {
@@ -15,11 +13,14 @@ const Highlights = () => {
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">Our Top insights.</h1>
-
-          
         </div>
-
         <VideoCarousel />
+        <div class="w-full p-4">
+          <div class="flex w-full">
+            <div class="w-1/2 bg-gray-300 p-4 text-center border">Box 1</div>
+            <div class="w-1/2 bg-gray-400 p-4 text-center border">Box 2</div>
+          </div>
+        </div>
       </div>
     </section>
   )
